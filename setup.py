e3f48cf61cb097448a700183419f85d5bb6cf069
rikonaka <xxy1836@gmail.com>
2018-12-13 17:26:44 +0800

    fix

--- setup.py
+++ setup.py
@@ -23,7 +23,7 @@ setuptools.setup(
 
     entry_points={
         "console_scripts": [
-            "k4c = katoolin.__main__:main",
+            "katlooin4china = katoolin4china.__main__:main",
         ],
     },
 
